21f7c2863283b899e4c804badcab30ae7c8c95d0
Giovanni Bezicheri <giovanni.bezicheri@rindus.de>
2022-02-19 17:06:54 +0000

    Add url validation using valid-url

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,5 +1,12 @@
-const validateUrl = () => {
-  return true; // TODO
+const validUrl = require('valid-url');
+
+const validateUrl = (url) => {
+  const valid = validUrl.isWebUri(url);
+  if (valid) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = { validateUrl };
